65e3f0fbb9d2380ce1d7a069e0bc838622a1d41f
Ashley Heestand <ashley@everydose.ai>
2023-01-24 11:20:00 -0600

    tests added

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,49 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { fireEvent, render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders inventory list", () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  expect(screen.getByText("Inventory List")).toBeInTheDocument();
+});
+
+test("navigates to edit inventory list", () => {
+  render(
+    <MemoryRouter>
+      <App />
+    </MemoryRouter>
+  );
+  fireEvent.click(screen.getAllByText("Edit List")[0]);
+  expect(screen.getByText("Your code goes here")).toBeInTheDocument();
+});
+
+test.skip("adds item to list", () => {
+  render(
+    <MemoryRouter initialEntries={["/lists"]}>
+      <App />
+    </MemoryRouter>
+  );
+  // Add your test here
+});
+
+test.skip("removes items from list", () => {
+  render(
+    <MemoryRouter initialEntries={["/lists"]}>
+      <App />
+    </MemoryRouter>
+  );
+  // Add your test here
+});
+
+test.skip("clears list", () => {
+  render(
+    <MemoryRouter initialEntries={["/lists"]}>
+      <App />
+    </MemoryRouter>
+  );
+  // Add your test here
 });
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,43 @@
 import React from "react";
-import { Table, Button } from "reactstrap";
+import { Table, Card, CardBody } from "reactstrap";
 
 const Home = (props) => {
   return (
     <div>
-      <h2 className="App-header">Inventory List</h2>
+      <h2 className="App-header mb-3">Inventory List</h2>
       <div className="d-flex justify-content-center">
-        <Table striped className="w-50">
-          <thead>
-            <tr>
-              <th>#</th>
-              <th>Item</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <th scope="row">1</th>
-              <td>Mark</td>
-            </tr>
-            <tr>
-              <th scope="row">2</th>
-              <td>Jacob</td>
-            </tr>
-            <tr>
-              <th scope="row">3</th>
-              <td>Larry</td>
-            </tr>
-          </tbody>
-        </Table>
+        <Card className="w-50">
+          <CardBody>
+            <Table striped className="">
+              <thead>
+                <tr>
+                  <th>#</th>
+                  <th>Item Name</th>
+                  <th>Quantity</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <th scope="row">1</th>
+                  <td>Item 1</td>
+                  <td>4</td>
+                </tr>
+                <tr>
+                  <th scope="row">2</th>
+                  <td>Item 2</td>
+                  <td>3</td>
+                </tr>
+                <tr>
+                  <th scope="row">3</th>
+                  <td>Item 3</td>
+                  <td>2</td>
+                </tr>
+              </tbody>
+            </Table>
+            <button className="btn btn-primary">Edit List</button>
+          </CardBody>
+        </Card>
       </div>
-      <Button>Edit List</Button>
     </div>
   );
 };
