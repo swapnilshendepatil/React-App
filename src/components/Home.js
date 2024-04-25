db2c6f63965ee8a579609e87079775194dd12636
Swapnil Shende <swapnilshende1998@gmail.com>
2024-04-25 16:47:03 +0530

    completed assignment

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import { Table, Card, CardBody } from "reactstrap";
 
 const Home = (props) => {
+  const [tableData] = useState([
+    { id: 1, itemName: "Item 1", quantity: 4 },
+    { id: 2, itemName: "Item 2", quantity: 3 },
+    { id: 3, itemName: "Item 3", quantity: 2 },
+  ]);
+
+  console.log("tableData in Home component:", tableData);
+
+  const handleEditClick = () => {
+    console.log(tableData);
+  };
+
   return (
     <div>
       <h2 className="App-header mb-3">Inventory List</h2>
@@ -17,28 +30,23 @@ const Home = (props) => {
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <th scope="row">1</th>
-                  <td>Item 1</td>
-                  <td>4</td>
-                </tr>
-                <tr>
-                  <th scope="row">2</th>
-                  <td>Item 2</td>
-                  <td>3</td>
-                </tr>
-                <tr>
-                  <th scope="row">3</th>
-                  <td>Item 3</td>
-                  <td>2</td>
-                </tr>
+                {tableData.map((item, index) => (
+                  <tr key={item.id}>
+                    <th scope="row">{index + 1}</th>
+                    <td>{item.itemName}</td>
+                    <td>{item.quantity}</td>
+                  </tr>
+                ))}
               </tbody>
             </Table>
-            <button className="btn btn-primary">Edit List</button>
+            <Link to={"/lists"} state={tableData}>
+              <button className="btn btn-primary">Edit List</button>
+            </Link>
           </CardBody>
         </Card>
       </div>
     </div>
   );
 };
+
 export { Home };
--- src/components/Lists.js
+++ src/components/Lists.js
@@ -1,17 +1,59 @@
-import React from "react";
-import { Card, CardBody, CardHeader } from "reactstrap";
+import React, { useState } from "react";
+import { Button, Card, CardBody, CardHeader } from "reactstrap";
+import { useLocation } from "react-router-dom";
 
 const Lists = (props) => {
+  const location = useLocation();
+  const tableData = location.state || {};
+  console.log(tableData);
+  const [inputValue, setInputValue] = useState("");
+  const [items, setItems] = useState(tableData);
+  const addItem = () => {
+    setItems([...items, { itemName: inputValue }]);
+    setInputValue("");
+  };
+  const removeItem = (index) => {
+    const updatedItems = items.filter((_, i) => i !== index);
+    setItems(updatedItems);
+  };
+  const clearItems = () => {
+    setItems([]);
+  };
+
   return (
     <div>
       <h2 className="App-header">Edit List</h2>
       <div className="d-flex justify-content-center">
         <Card className="w-50">
-          <CardHeader>
-            Edit <code>src/App.js</code> and save to reload.
+          <CardHeader className="cardheader">
+            <h6 className="list-header">Item Name</h6>
+            <div className="list-div">
+              <input
+                className="input"
+                value={inputValue}
+                onChange={(e) => setInputValue(e.target.value)}
+              />
+              <Button className="btn btn-primary" onClick={addItem}>
+                Add
+              </Button>
+            </div>
+            <span>To get started,add 1 or more items</span>
           </CardHeader>
           <CardBody>
-            <p>Your code goes here</p>
+            {items.map((data, index) => (
+              <>
+                <div key={index} className="tabledata">
+                  <h6>{data.itemName}</h6>
+                  <span onClick={() => removeItem(index)}>x</span>
+                </div>
+                <hr />
+              </>
+            ))}
+            {items.length !== 0 && (
+              <Button outline className="clrBtn" onClick={clearItems}>
+                Clear All
+              </Button>
+            )}
           </CardBody>
         </Card>
       </div>
